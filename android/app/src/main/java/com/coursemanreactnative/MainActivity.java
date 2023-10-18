56c7106a8deb57879bc95f53041b7d623818bbf5
linh.tq <tranquanglinh.pt@gmail.com>
2023-10-18 18:54:03 +0700

    Update reactnative to 0.76.

--- android/app/src/main/java/com/coursemanreactnative/MainActivity.java
+++ android/app/src/main/java/com/coursemanreactnative/MainActivity.java
@@ -1,25 +1,32 @@
 package com.coursemanreactnative;
 
 import com.facebook.react.ReactActivity;
-//react navigation
-import android.os.Bundle;
-//react navigation
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
+import com.facebook.react.defaults.DefaultReactActivityDelegate;
+
 public class MainActivity extends ReactActivity {
 
   /**
-   * Returns the name of the main component registered from JavaScript. This is
-   * used to schedule
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-
   @Override
   protected String getMainComponentName() {
     return "CourseManReactNative";
   }
-  //react navigation
+
+  /**
+   * Returns the instance of the {@link ReactActivityDelegate}. Here we use a util class {@link
+   * DefaultReactActivityDelegate} which allows you to easily enable Fabric and Concurrent React
+   * (aka React 18) with two boolean flags.
+   */
   @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(null);
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new DefaultReactActivityDelegate(
+        this,
+        getMainComponentName(),
+        // If you opted-in for the New Architecture, we enable the Fabric Renderer.
+        DefaultNewArchitectureEntryPoint.getFabricEnabled());
   }
-  //react navigation
 }
